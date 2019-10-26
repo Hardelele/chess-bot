d2b6ace7ebe8bd1b2fd258652e778d42f1501271
Hardelele <hardelele@yahoo.com>
2019-10-26 11:50:05 +0300

    Add empty King Figure

---
+++ src/com/github/hardelele/chess/chessboard/figures/impl/KingFigure.java
@@ -0,0 +1,34 @@
+package com.github.hardelele.chess.chessboard.figures.impl;
+
+import com.github.hardelele.chess.chessboard.Coords;
+import com.github.hardelele.chess.chessboard.Square;
+import com.github.hardelele.chess.chessboard.figures.Figure;
+
+import java.util.HashMap;
+
+public class KingFigure implements Figure {
+    @Override
+    public Coords getCoords() {
+        return null;
+    }
+
+    @Override
+    public void setCoords(Coords coords) {
+
+    }
+
+    @Override
+    public HashMap<String, Square> getAbleMoves() {
+        return null;
+    }
+
+    @Override
+    public void countAbleMoves(HashMap<String, Square> boardState) {
+
+    }
+
+    @Override
+    public void printAllAbleMoves() {
+
+    }
+}
