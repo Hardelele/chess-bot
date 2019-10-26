61576c3b1a09c6f77e9289224a6ae51ee2a7434a
Hardelele <hardelele@yahoo.com>
2019-10-26 14:07:55 +0300

    Clean code in PawnFigure class

--- src/com/github/hardelele/chess/chessboard/figures/impl/PawnFigure.java
+++ src/com/github/hardelele/chess/chessboard/figures/impl/PawnFigure.java
@@ -4,11 +4,12 @@ import com.github.hardelele.chess.chessboard.BoardState;
 import com.github.hardelele.chess.chessboard.Coords;
 import com.github.hardelele.chess.chessboard.Square;
 import com.github.hardelele.chess.chessboard.figures.Figure;
+import com.github.hardelele.chess.chessboard.figures.FigureImpl;
 
 import java.util.HashMap;
 import java.util.StringTokenizer;
 
-public class PawnFigure implements Figure {
+public class PawnFigure extends FigureImpl {
 
     private Coords coords;
 
@@ -25,24 +26,15 @@ public class PawnFigure implements Figure {
         this.coords = new Coords(number,letter);
     }
 
-    public Coords getCoords() {
-        return coords;
-    }
+    @Override
+    public void countAbleMoves(BoardState boardState) {
 
-    public void setCoords(Coords coords) {
-        this.coords = coords;
-    }
-
-    public HashMap<String, Square> getAbleMoves() {
-        return ableMoves;
-    }
-
-    public void countAbleMoves(HashMap<String, Square> boardState) {
+        HashMap<String,Square> squares = boardState.getSquares();
 
         String thisLetter = this.coords.getLetter();
         int thisNumber = this.coords.getNumber();
 
-        StringTokenizer stringTokenizer = new StringTokenizer(BoardState.letters);
+        StringTokenizer stringTokenizer = new StringTokenizer(boardState.letters);
         String nextLetter = null;
 
         while (stringTokenizer.nextToken() != thisLetter) {
@@ -52,19 +44,19 @@ public class PawnFigure implements Figure {
 
         Square leftFlank = null;
         Square rightFlank = null;
-        Square nextSquare = boardState.get(nextLetter + thisNumber);
+        Square nextSquare = squares.get(nextLetter + thisNumber);
 
         HashMap<String, Square> ableMoves = new HashMap<>();
 
         if (thisNumber!=1) {
-            leftFlank = boardState.get(nextLetter + (thisNumber - 1));
+            leftFlank = squares.get(nextLetter + (thisNumber - 1));
             if (leftFlank.getFigure() != null) {
                 ableMoves.put(nextLetter + (thisNumber - 1), leftFlank);
             }
         }
 
         if (thisNumber!=8) {
-            rightFlank = boardState.get(nextLetter + (thisNumber + 1));
+            rightFlank = squares.get(nextLetter + (thisNumber + 1));
             if (rightFlank.getFigure() != null) {
                 ableMoves.put(nextLetter + (thisNumber - 1), rightFlank);
             }
@@ -75,11 +67,5 @@ public class PawnFigure implements Figure {
         }
 
         this.ableMoves = ableMoves;
-    }
-
-    public void printAllAbleMoves() {
-        for (Square value : ableMoves.values()) {
-            System.out.println(value.getCoords().toString());
-        }
     }
 }
