951e168d1e649c42e368446a0d49cbb17bef1ceb
Hardelele <hardelele@yahoo.com>
2019-10-26 11:50:54 +0300

    Add methods from Pawn to Figure

--- src/com/github/hardelele/chess/chessboard/figures/Figure.java
+++ src/com/github/hardelele/chess/chessboard/figures/Figure.java
@@ -1,5 +1,15 @@
 package com.github.hardelele.chess.chessboard.figures;
 
+import com.github.hardelele.chess.chessboard.Coords;
+import com.github.hardelele.chess.chessboard.Square;
+
+import java.util.HashMap;
+
 public interface Figure {
 
+    Coords getCoords();
+    void setCoords(Coords coords);
+    HashMap<String, Square> getAbleMoves();
+    void countAbleMoves(HashMap<String, Square> boardState);
+    void printAllAbleMoves();
 }
