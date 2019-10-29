40b634bfe2dca008476c9074766e9675d31cfc79
Hardelele <i.lapshina@mail.ru>
2019-10-29 07:01:56 +0300

    Square: Add variables, constructors, get&set. - Add constructors for any type of data. - Add variables: Coordinates coordinates and Figure figure - Add getters and setters for this two variables.

--- src/com/github/hardelele/chess/chessboard/objects/Square.java
+++ src/com/github/hardelele/chess/chessboard/objects/Square.java
@@ -1,4 +1,71 @@
 package com.github.hardelele.chess.chessboard.objects;
 
+import com.github.hardelele.chess.chessboard.figures.Figure;
+
+/*
+ TODO:: Make documentation for this code.
+ TODO:: Rewrite code from git.
+ */
+
 public class Square {
+
+    Coordinates coordinates;
+
+    Figure figure;
+
+    public Square() {
+        setFigure(null);
+        setCoordinates(new Coordinates());
+    }
+
+    public Square(String coords) {
+        setFigure(null);
+        setCoordinates(new Coordinates(coords));
+    }
+
+    public Square(int number, String letter) {
+        setFigure(null);
+        setCoordinates(new Coordinates(number,letter));
+    }
+
+    public Square(Coordinates coordinates) {
+        setFigure(null);
+        setCoordinates(coordinates);
+    }
+
+    public Square(Figure figure) {
+        setFigure(figure);
+        setCoordinates(new Coordinates());
+    }
+
+    public Square(int number, String letter, Figure figure) {
+        setFigure(figure);
+        setCoordinates(new Coordinates(number,letter));
+    }
+
+    public Square(Coordinates coordinates, Figure figure) {
+        setFigure(figure);
+        setCoordinates(coordinates);
+    }
+
+    public Square(String coords, Figure figure) {
+        setFigure(figure);
+        setCoordinates(new Coordinates(coords));
+    }
+
+    public Coordinates getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(Coordinates coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
+
+    public void setFigure(Figure figure) {
+        this.figure = figure;
+    }
 }
