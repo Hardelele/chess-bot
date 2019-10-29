48353dbbc904d10321523d9b52f6f77844af3ed1
Hardelele <hardelele@yahoo.com>
2019-10-29 18:07:06 +0200

    Implementation FigureInterface in Figure

--- src/com/github/hardelele/chess/chessboard/figures/Figure.java
+++ src/com/github/hardelele/chess/chessboard/figures/Figure.java
@@ -1,4 +1,16 @@
 package com.github.hardelele.chess.chessboard.figures;
 
-public abstract class Figure {
+import com.github.hardelele.chess.chessboard.StateOfChessboard;
+import com.github.hardelele.chess.chessboard.objects.Square;
+
+import java.util.HashMap;
+
+public abstract class Figure implements FigureInterface {
+
+    private HashMap<String, Square> ableMoves;
+
+    @Override
+    public HashMap<String, Square> getAbleMoves() {
+        return ableMoves;
+    }
 }
