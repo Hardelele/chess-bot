75e12eef19d5a20550d7e943ae817a09f29d0546
Hardelele <hardelele@yahoo.com>
2019-10-29 18:05:53 +0200

    Add methods to FigureInterface - getAbleMoves() - findAbleMoves()

--- src/com/github/hardelele/chess/chessboard/figures/FigureInterface.java
+++ src/com/github/hardelele/chess/chessboard/figures/FigureInterface.java
@@ -1,4 +1,11 @@
 package com.github.hardelele.chess.chessboard.figures;
 
+import com.github.hardelele.chess.chessboard.StateOfChessboard;
+import com.github.hardelele.chess.chessboard.objects.Square;
+
+import java.util.HashMap;
+
 public interface FigureInterface {
+    HashMap<String, Square> getAbleMoves();
+    void findAbleMoves(StateOfChessboard chessboard);
 }
