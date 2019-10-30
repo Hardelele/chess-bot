a76206885f941cc5fb4e1c8d3587ddaf418453b9
Hardelele <i.lapshina@mail.ru>
2019-10-31 00:36:00 +0300

    StateOfChessboard: Add constructor and methods - Constructor call all new methods:           setPawnsAsDebutStarting();           setBishopsAsDebutStarting();           setKingsAsDebutStarting();           setKnightsAsDebutStarting();           setQueensAsDebutStarting();           setRookAsDebutStarting();

--- src/com/github/hardelele/chess/visualize/ChessApplication.java
+++ src/com/github/hardelele/chess/visualize/ChessApplication.java
@@ -1,13 +1,22 @@
 package com.github.hardelele.chess.visualize;
 
 import javafx.application.Application;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+
 public class ChessApplication extends Application {
+
+    public static final ArrayList<String> letters = new ArrayList<String>() {{
+        add("A");
+        add("B");
+        add("C");
+        add("D");
+        add("E");
+        add("F");
+        add("G");
+        add("H");
+    }};
 
     public static void main(String[] args) {
         launch(args);
