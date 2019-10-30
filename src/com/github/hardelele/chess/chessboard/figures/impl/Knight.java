199d3804dae1f27d357c7d426976be39924f1047
Hardelele <i.lapshina@mail.ru>
2019-10-31 00:32:27 +0300

    Knight extends FigureImpl instead Figure - Also add constructor, that send params in super()

--- src/com/github/hardelele/chess/chessboard/figures/impl/Knight.java
+++ src/com/github/hardelele/chess/chessboard/figures/impl/Knight.java
@@ -1,6 +1,10 @@
 package com.github.hardelele.chess.chessboard.figures.impl;
 
-import com.github.hardelele.chess.chessboard.figures.Figure;
+import com.github.hardelele.chess.chessboard.figures.FigureImpl;
 
-public class Knight extends Figure {
+public class Knight extends FigureImpl {
+
+    public Knight(String color, int letterCoordinateNumber, int numberCoordinate) {
+        super(color, letterCoordinateNumber, numberCoordinate);
+    }
 }
