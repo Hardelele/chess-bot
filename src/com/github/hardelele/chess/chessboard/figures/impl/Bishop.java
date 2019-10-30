6d707711c81e1dc552619ed095961aa58b629676
Hardelele <i.lapshina@mail.ru>
2019-10-31 00:31:53 +0300

    Bishop extends FigureImpl instead Figure - Also add constructor, that send params in super()

--- src/com/github/hardelele/chess/chessboard/figures/impl/Bishop.java
+++ src/com/github/hardelele/chess/chessboard/figures/impl/Bishop.java
@@ -1,6 +1,10 @@
 package com.github.hardelele.chess.chessboard.figures.impl;
 
-import com.github.hardelele.chess.chessboard.figures.Figure;
+import com.github.hardelele.chess.chessboard.figures.FigureImpl;
 
-public class Bishop extends Figure {
+public class Bishop extends FigureImpl {
+
+    public Bishop(String color, int letterCoordinateNumber, int numberCoordinate) {
+        super(color, letterCoordinateNumber, numberCoordinate);
+    }
 }
