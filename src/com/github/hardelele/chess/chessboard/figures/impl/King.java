67feaecf901eae174702d6381380f005593d6095
Hardelele <i.lapshina@mail.ru>
2019-10-31 00:32:15 +0300

    King extends FigureImpl instead Figure - Also add constructor, that send params in super()

--- src/com/github/hardelele/chess/chessboard/figures/impl/King.java
+++ src/com/github/hardelele/chess/chessboard/figures/impl/King.java
@@ -1,6 +1,10 @@
 package com.github.hardelele.chess.chessboard.figures.impl;
 
-import com.github.hardelele.chess.chessboard.figures.Figure;
+import com.github.hardelele.chess.chessboard.figures.FigureImpl;
 
-public class King extends Figure {
+public class King extends FigureImpl {
+
+    public King(String color, int letterCoordinateNumber, int numberCoordinate) {
+        super(color, letterCoordinateNumber, numberCoordinate);
+    }
 }
