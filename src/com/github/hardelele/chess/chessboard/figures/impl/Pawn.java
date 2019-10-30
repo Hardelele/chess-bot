666370422e9b13c99b7eab3c360e327038eff557
Hardelele <i.lapshina@mail.ru>
2019-10-31 00:32:41 +0300

    Pawn extends FigureImpl instead Figure - Also add constructor, that send params in super()

--- src/com/github/hardelele/chess/chessboard/figures/impl/Pawn.java
+++ src/com/github/hardelele/chess/chessboard/figures/impl/Pawn.java
@@ -1,7 +1,10 @@
 package com.github.hardelele.chess.chessboard.figures.impl;
 
-import com.github.hardelele.chess.chessboard.figures.Figure;
+import com.github.hardelele.chess.chessboard.figures.FigureImpl;
 
-public class Pawn extends Figure {
+public class Pawn extends FigureImpl {
 
+    public Pawn(String color, int letterCoordinateNumber, int numberCoordinate) {
+        super(color, letterCoordinateNumber, numberCoordinate);
+    }
 }
