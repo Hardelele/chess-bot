230fb5dbdff522520bcab513dc59d0315fe1bd4d
Hardelele <i.lapshina@mail.ru>
2019-10-31 00:33:13 +0300

    Rock extends FigureImpl instead Figure - Also add constructor, that send params in super()

--- src/com/github/hardelele/chess/chessboard/figures/impl/Rock.java
+++ src/com/github/hardelele/chess/chessboard/figures/impl/Rock.java
@@ -1,7 +1,10 @@
 package com.github.hardelele.chess.chessboard.figures.impl;
 
-import com.github.hardelele.chess.chessboard.figures.Figure;
+import com.github.hardelele.chess.chessboard.figures.FigureImpl;
 
-public class Rock extends Figure {
+public class Rock extends FigureImpl {
 
+    public Rock(String color, int letterCoordinateNumber, int numberCoordinate) {
+        super(color, letterCoordinateNumber, numberCoordinate);
+    }
 }
