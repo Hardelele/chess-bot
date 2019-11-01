4b6798d4474e28828ace223aa8af0e1885f4f30b
Hardelele <i.lapshina@mail.ru>
2019-11-01 08:22:22 +0300

    Fix packages and imports in figure impl classes

--- src/com/github/hardelele/chess/figures/impl/Rock.java
+++ src/com/github/hardelele/chess/figures/impl/Rock.java
@@ -1,6 +1,6 @@
-package com.github.hardelele.chess.chessboard.figures.impl;
+package com.github.hardelele.chess.figures.impl;
 
-import com.github.hardelele.chess.chessboard.figures.FigureImpl;
+import com.github.hardelele.chess.figures.FigureImpl;
 
 public class Rock extends FigureImpl {
 
