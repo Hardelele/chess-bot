9313405f597136e1fd71c832d3a0b32a4dc121db
Hardelele <hardelele@yahoo.com>
2019-11-03 09:53:19 +0200

    Edit figuresList in StateOfChessboardImpl Divided figuresList into two Arrays: blackFigures and whiteFigures

--- src/com/github/hardelele/chess/chessbords/StateOfChessboardImpl.java
+++ src/com/github/hardelele/chess/chessbords/StateOfChessboardImpl.java
@@ -3,45 +3,72 @@ package com.github.hardelele.chess.chessbords;
 import com.github.hardelele.chess.figures.Figure;
 import com.github.hardelele.chess.figures.impl.*;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 
-public abstract class StateOfChessboardImpl implements StateOfChessboard, Serializable {
+public abstract class StateOfChessboardImpl implements StateOfChessboard {
 
-    private ArrayList<Figure> figuresList = new ArrayList<>();
+    private ArrayList<Figure> whiteFigures = new ArrayList<>();
+    private ArrayList<Figure> blackFigures = new ArrayList<>();
 
     @Override
     public ArrayList<Figure> getFiguresList() {
+        ArrayList<Figure> figuresList = new ArrayList<>();
+        figuresList.addAll(whiteFigures);
+        figuresList.addAll(blackFigures);
         return figuresList;
     }
 
     @Override
     public void addPawn(String color, int letterCoordinateNumber, int numberCoordinate) {
-        figuresList.add(new Pawn(color,letterCoordinateNumber,numberCoordinate));
+        if (color.equals("white")) {
+            whiteFigures.add(new Pawn(color,letterCoordinateNumber,numberCoordinate));
+        } else {
+            blackFigures.add(new Pawn(color,letterCoordinateNumber,numberCoordinate));
+        }
     }
 
     @Override
     public void addQueen(String color, int letterCoordinateNumber, int numberCoordinate) {
-        figuresList.add(new Queen(color,letterCoordinateNumber,numberCoordinate));
+        if (color.equals("white")) {
+            whiteFigures.add(new Queen(color,letterCoordinateNumber,numberCoordinate));
+        } else {
+            blackFigures.add(new Queen(color,letterCoordinateNumber,numberCoordinate));
+        }
     }
 
     @Override
     public void addBishop(String color, int letterCoordinateNumber, int numberCoordinate) {
-        figuresList.add(new Bishop(color,letterCoordinateNumber,numberCoordinate));
+        if (color.equals("white")) {
+            whiteFigures.add(new Bishop(color,letterCoordinateNumber,numberCoordinate));
+        } else {
+            blackFigures.add(new Bishop(color,letterCoordinateNumber,numberCoordinate));
+        }
     }
 
     @Override
     public void addKnight(String color, int letterCoordinateNumber, int numberCoordinate) {
-        figuresList.add(new Knight(color,letterCoordinateNumber,numberCoordinate));
+        if (color.equals("white")) {
+            whiteFigures.add(new Knight(color,letterCoordinateNumber,numberCoordinate));
+        } else {
+            blackFigures.add(new Knight(color,letterCoordinateNumber,numberCoordinate));
+        }
     }
 
     @Override
     public void addRook(String color, int letterCoordinateNumber, int numberCoordinate) {
-        figuresList.add(new Rock(color,letterCoordinateNumber,numberCoordinate));
+        if (color.equals("white")) {
+            whiteFigures.add(new Rock(color,letterCoordinateNumber,numberCoordinate));
+        } else {
+            blackFigures.add(new Rock(color,letterCoordinateNumber,numberCoordinate));
+        }
     }
 
     @Override
     public void addKing(String color, int letterCoordinateNumber, int numberCoordinate){
-        figuresList.add(new King(color,letterCoordinateNumber,numberCoordinate));
+        if (color.equals("white")) {
+            whiteFigures.add(new King(color,letterCoordinateNumber,numberCoordinate));
+        } else {
+            blackFigures.add(new King(color,letterCoordinateNumber,numberCoordinate));
+        }
     }
 }
