6480988459a4e0a842655aec0bd2f4e85ff4bf12
Hardelele <hardelele@yahoo.com>
2019-11-03 09:59:40 +0200

    StatesSerializer has been implemented - Realize methods that save and load state of chessboard.

--- src/com/github/hardelele/chess/StatesSerializer.java
+++ src/com/github/hardelele/chess/StatesSerializer.java
@@ -1,7 +1,23 @@
 package com.github.hardelele.chess;
 
+import com.github.hardelele.chess.chessbords.StateOfChessboard;
+
+import java.io.*;
+
 public class StatesSerializer {
 
-    
+    public void save(StateOfChessboard state, String fileName) throws IOException {
+        FileOutputStream fileOutputStream = new FileOutputStream(fileName);
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+        objectOutputStream.writeObject(state);
+        objectOutputStream.close();
+    }
 
+    public StateOfChessboard load(String fileName) throws IOException, ClassNotFoundException {
+        FileInputStream fileInputStream = new FileInputStream(fileName);
+        ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+        StateOfChessboard state = (StateOfChessboard) objectInputStream.readObject();
+        objectInputStream.close();
+        return state;
+    }
 }
