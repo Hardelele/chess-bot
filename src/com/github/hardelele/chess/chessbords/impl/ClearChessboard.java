c46c20df587c0992241c4b7229dd43b2a901784c
Hardelele <hardelele@yahoo.com>
2019-11-03 09:48:46 +0200

    Add class ClearChessboard State of chessboard with 2 kings only.

---
+++ src/com/github/hardelele/chess/chessbords/impl/ClearChessboard.java
@@ -0,0 +1,52 @@
+package com.github.hardelele.chess.chessbords.impl;
+
+import com.github.hardelele.chess.chessbords.StateOfChessboardImpl;
+
+public class ClearChessboard extends StateOfChessboardImpl {
+
+    public ClearChessboard() {
+        putFigures();
+    }
+
+    private void putFigures() {
+        putKings();
+    }
+
+    private void putPawns() {
+        for (int counter = 1; counter < 9; counter++) {
+            addPawn("white",2,counter);
+            addPawn("black",7,counter);
+        }
+    }
+
+    private void putQueens() {
+        addQueen("white",1,4);
+        addQueen("black",8,5);
+    }
+
+    private void putBishops() {
+        for (int counter = 3; counter < 9; counter+=3) {
+            addBishop("white",1,counter);
+            addBishop("black",8,counter);
+        }
+    }
+
+    private void putKnights() {
+        for (int counter = 2; counter < 9; counter+=5) {
+            addKnight("white",1,counter);
+            addKnight("black",8,counter);
+        }
+    }
+
+    private void putRooks() {
+        for (int counter = 1; counter < 9; counter+=7) {
+            addRook("white",1,counter);
+            addRook("black",8,counter);
+        }
+    }
+
+    private void putKings(){
+        addKing("white",1,5);
+        addKing("black",8,4);
+    }
+}
