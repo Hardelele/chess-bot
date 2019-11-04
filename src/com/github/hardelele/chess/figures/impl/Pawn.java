ea6bd9f6c392b6a1bb33c02529addf66914478c4
Hardelele <i.lapshina@mail.ru>
2019-11-04 16:21:10 +0300

    Pawn: tried to implement calculateAllMoves method - its didn’t work :b

--- src/com/github/hardelele/chess/figures/impl/Pawn.java
+++ src/com/github/hardelele/chess/figures/impl/Pawn.java
@@ -1,10 +1,27 @@
 package com.github.hardelele.chess.figures.impl;
 
+import com.github.hardelele.chess.chessbords.impl.Coordinates;
 import com.github.hardelele.chess.figures.FigureImpl;
+
+import java.util.ArrayList;
 
 public class Pawn extends FigureImpl {
 
-    public Pawn(String color, int letterCoordinateNumber, int numberCoordinate) {
-        super(color, letterCoordinateNumber, numberCoordinate);
+    public Pawn(int id, String color, int letterCoordinateNumber, int numberCoordinate) {
+        super(id, color, letterCoordinateNumber, numberCoordinate);
+    }
+
+    @Override
+    public void calculateAllMoves(ArrayList<String> movesList) {
+        for (int counter = getNumericCoordinateNumber()-1; counter <= getNumericCoordinateNumber()+1; counter++) {
+            tryAddMove(new Coordinates(getLetterCoordinateNumber()+1,counter));
+        }
+    }
+
+    private void tryAddMove(Coordinates moveCoords) {
+        // compare to enemy or friendly figure
+        if(!isChessboardEnd(moveCoords)) {
+            
+        }
     }
 }
